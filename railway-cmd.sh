0f0db22c9a51f74ba267778bc64a38bac6600781
MN Jewel <174652026+W3JDev@users.noreply.github.com>
2026-03-17 15:00:12 +0800

    fix: inject DB_HOST into common_site_config on every startup

--- railway-cmd.sh
+++ railway-cmd.sh
@@ -4,6 +4,9 @@ set -e
 echo "-> Clearing cache"
 su frappe -c "bench execute frappe.cache_manager.clear_global_cache"
 
+echo "-> Ensure DB host is set in common site config"
+echo "{\"db_host\": \"${DB_HOST}\"}" > /home/frappe/bench/sites/common_site_config.json
+
 echo "-> Bursting env into config"
 envsubst '$RFP_DOMAIN_NAME' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
 envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/$systemUser/temp_supervisor.conf > /home/$systemUser/supervisor.conf
