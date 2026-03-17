ad86577c7c50eee76ef125499bd5bd5489ac5a69
MN Jewel <174652026+W3JDev@users.noreply.github.com>
2026-03-17 14:59:22 +0800

    fix: inject DB_HOST into common_site_config and install hrms instead of erpnext

--- railway-setup.sh
+++ railway-setup.sh
@@ -6,11 +6,11 @@ set -e
 # it doesn't run entrypoint first, so we need to run it here.
 sudo /usr/local/bin/railway-entrypoint.sh
 
-echo "-> Create empty common site config"
-echo "{}" > /home/frappe/bench/sites/common_site_config.json
+echo "-> Write DB host into common site config"
+echo "{\"db_host\": \"${DB_HOST}\"}" > /home/frappe/bench/sites/common_site_config.json
 
-echo "-> Create new site with ERPNext"
-bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${RFP_DB_ROOT_PASSWORD} --install-app erpnext
+echo "-> Create new site with HRMS"
+bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${RFP_DB_ROOT_PASSWORD} --db-host ${DB_HOST} --install-app hrms
 bench use ${RFP_DOMAIN_NAME}
 
 echo "-> Enable scheduler"
